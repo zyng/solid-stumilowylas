c186b7282f8c1e6889c62744ba3a26fe6c8dfae3
zyng <kisielewskimichal@gmail.com>
2018-01-20 18:17:02 +0100

    #reek: attibute, instancevariableassumption, irresponsiblemodule, uncommunicativevariablename remove from compound_word.rb

--- compound_word.rb
+++ compound_word.rb
@@ -1,10 +1,13 @@
 require_relative "word"
-
+#CompoundWord class
 class CompoundWord < Word 
 	
-	attr_accessor :x, :y, :points
-	
+	attr_reader :y, :points
+
 	def initialize(string)
+		@position_y=position_y
+		@vel=vel
+		@points=points
 		super(string)
 		@color = Gosu::Color.new(0xff_00ff00)
 		@points = 20 
@@ -12,7 +15,7 @@ class CompoundWord < Word
 
 	def move
 		@vel = @vel + 0.5 * rand(0.05..0.07)#This is the acceleration
-		@y += @vel * 0.25
+		@yposition_y += @position_y * 0.25
 	end
 	
 end
